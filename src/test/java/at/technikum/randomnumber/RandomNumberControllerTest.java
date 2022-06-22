e8d31bdd1a4700865b956f52cc82606fd1a031a1
Herwig Brunnthaler <wi21b113@technikum-wien.at>
2022-06-22 18:02:46 +0200

    Changed Test

--- src/test/java/at/technikum/randomnumber/RandomNumberControllerTest.java
+++ src/test/java/at/technikum/randomnumber/RandomNumberControllerTest.java
@@ -1,14 +1,20 @@
 package at.technikum.randomnumber;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class RandomNumberControllerTest {
-/*
+
+    /*
+
     @Test
     void randomNumberTrue() {
         Random rn = new Random();
         int toProveOk = rn.nextInt(1,101);
-        Assertions.assertTrue(toProveOk > 0 && toProveOk < 101);
+        Assertions.assertEquals(toProveOk < 101,toProveOk);
+        }
     }
+
 
     @Test
     void randomNumberFalse() {
