fd5dfe090630e2e5e29d15be2b971d8a014824b0
Herwig Brunnthaler <wi21b113@technikum-wien.at>
2022-06-22 14:16:12 +0200

    Initial commit

---
+++ src/main/java/at/technikum/randomnumber/RandomNumberApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.randomnumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomNumberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomNumberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/technikum/randomnumber/RandomNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.randomnumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomNumberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
